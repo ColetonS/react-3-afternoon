2845f4dad939afa49bae99a1fb7b44e4c34bb69b
ColetonS <coleton.m.smith@gmail.com>
2019-07-11 15:00:36 -0600

    my message

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
+
+import axios from 'axios'
 
 class App extends Component {
   constructor() {
@@ -19,15 +22,22 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts').then(results => {
+      this.setState({posts: results.data})
+    })
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text}).then(results => {
+      this.setState({posts: results.data})
+    })
   
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then( results => {
+      this.setState({ posts: results.data })
+    })
   }
 
   createPost() {
@@ -45,6 +55,17 @@ class App extends Component {
 
           <Compose />
           
+          {
+            posts.map(post => (
+              <Post key={post.id} 
+                    text={post.text}
+                    date={post.date}
+                    id={post.id}
+                    updatePostFn={this.updatePost}
+                    deletePostFn={ this.deletePost } />
+            ))
+          }
+
         </section>
       </div>
     );
